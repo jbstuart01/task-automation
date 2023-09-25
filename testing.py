8366be27f58c328d3fc91ec8652570410651fa94
jbstuart01 <145608139+jbstuart01@users.noreply.github.com>
2023-09-25 15:06:50 -0400

    added step to check_phone_status

--- testing.py
+++ testing.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 
 # open a Google Chrome browser and call it 'browser'
 browser = webdriver.Chrome()
@@ -13,10 +14,11 @@ browser = webdriver.Chrome()
 # default email to use as username/email for logging in
 my_email = "jacob.stuart@gtkycu.com"
 
-def get_element(driver, by, keyword):
+# returns a web element, "by" = type of field, "keyword" = unique ID
+def get_element(by, keyword):
     # wait until the page has loaded properly,
     # when the specified ID is visible
-    wait = WebDriverWait(driver, 5)
+    wait = WebDriverWait(browser, 5)
 
     if by == "ID" or by == "id" or by == "Id" or by == "iD":
         # returns the element that has the corresponding ID
@@ -24,18 +26,24 @@ def get_element(driver, by, keyword):
     elif by == "NAME" or by == "Name" or by == "name":
         # returns the element that has the corresponding ID
         element = wait.until(EC.presence_of_element_located((By.NAME, keyword)))
-    
+    elif by == "CLASS" or by == "class" or by == "Class":
+        # returns the element that has the corresponding ID
+        element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, keyword)))
+    elif by == "TAG" or by == "tag" or by == "Tag":
+        # returns the element that has the corresponding ID
+        element = wait.until(EC.presence_of_element_located((By.TAG_NAME, keyword)))
+
     return element
 
-# automatically upload Participation Tracking
+# upload Participation Tracking
 def participation_tracking(user_ID, password):
     # go to TruStage/CUNA
     browser.get('https://myservices.cunamutual.com/')
     
     # find username box
-    user_ID_box = get_element(browser, ID, "okta-signin-username")
+    user_ID_box = get_element(browser, "ID", "okta-signin-username")
     # find password box
-    pw_box = get_element(browser, ID, "okta-signin-password")
+    pw_box = get_element(browser, "ID", "okta-signin-password")
 
     # enter user ID
     user_ID_box.send_keys(user_ID)
@@ -46,28 +54,45 @@ def participation_tracking(user_ID, password):
     
     # need to deal with email verification
 
-# automatically check online banking
+# check online banking
 def check_online_banking(user_ID, password):
     # go to GTKYCU's website
     browser.get('https://gtkycu.com/')
 
     # get the user ID
-    user_ID = get_element(browser, NAME, "UserName")
+    user_ID_box = get_element(browser, "NAME", "UserName")
     # get the password
-    pw = get_element(browser, NAME, "Password")
-    
-# automatically dial main line
+    pw = get_element(browser, "NAME", "Password")
+
+    # get the right side "Online Banking" menu
+    get_element(browser, "CLASS", "olbIcon").moveToElement()
+
+    # enter the username and password
+    user_ID_box.send_keys(user_ID)
+    pw.send_keys(password)
+
+    time.sleep(3)
+
+# dial main line
 def check_phone_status(username, password):
+    # go to OfficeSuite
     browser.get('https://login.windstream.com/idp/startSSO.ping?PartnerSpId=https%3A%2F%2Flogin.windstream.com&loginAdapterId=myBusinessWeb&TargetResource=https://mybusiness.gokinetic.com/account/pingauth/po3login')
 
-     # don't do anything until the page has loaded properly
-    # we can tell it's loaded when the User ID is clickable
-    # if it's not loaded after 4 seconds, throw TimeOutException
-    wait = WebDriverWait(browser, 4)
-    element = wait.until(EC.presence_of_element_located((By.NAME, "UserName")))
+    # enter username and password
+    get_element("ID", "username").send_keys(username)
+    get_element("ID", "password").send_keys(password)
+
+    # click sign on button
+    get_element("ID", "signOnButton").click()    
+    # click "Go To OfficeSuite"
+    get_element("CLASS", "smb-btn-primary").click()
+    # click "Dialpad"
+    #get_element("TAG", "dialpad").moveToElement()
+    time.sleep(5)
+
 
 def main():
     #participation_tracking("jbstuart", "Frosty-Shelving83")
-    check_online_banking("TestAccount2", "Twisty-Denial-Hymn80")
-
+    #check_online_banking("TestAccount2", "Twisty-Denial-Hymn80")
+    check_phone_status(my_email, "Dusty-Star-Fire34")
 main()
