4c265f6a7e3e2cb3d8eb0581cfcb9e12736001fb
jbstuart01 <145608139+jbstuart01@users.noreply.github.com>
2023-09-25 14:18:30 -0400

    added get_element function

--- testing.py
+++ testing.py
@@ -13,28 +13,36 @@ browser = webdriver.Chrome()
 # default email to use as username/email for logging in
 my_email = "jacob.stuart@gtkycu.com"
 
+def get_element(driver, by, keyword):
+    # wait until the page has loaded properly,
+    # when the specified ID is visible
+    wait = WebDriverWait(driver, 5)
+
+    if by == "ID" or by == "id" or by == "Id" or by == "iD":
+        # returns the element that has the corresponding ID
+        element = wait.until(EC.presence_of_element_located((By.ID, keyword)))
+    elif by == "NAME" or by == "Name" or by == "name":
+        # returns the element that has the corresponding ID
+        element = wait.until(EC.presence_of_element_located((By.NAME, keyword)))
+    
+    return element
+
 # automatically upload Participation Tracking
 def participation_tracking(user_ID, password):
-    # go to TruStage
+    # go to TruStage/CUNA
     browser.get('https://myservices.cunamutual.com/')
     
-    # don't do anything until the page has loaded properly
-    # we can tell it's loaded when the User ID is clickable
-    # if it's not loaded after 4 seconds, throw TimeOutException
-    wait = WebDriverWait(browser, 4)
-    element = wait.until(EC.element_to_be_clickable((By.ID, "okta-signin-username")))
-
     # find username box
-    user_ID_box = browser.find_element(By.ID, "okta-signin-username")
+    user_ID_box = get_element(browser, ID, "okta-signin-username")
     # find password box
-    pw_box = browser.find_element(By.ID, "okta-signin-password")
+    pw_box = get_element(browser, ID, "okta-signin-password")
 
     # enter user ID
     user_ID_box.send_keys(user_ID)
     # enter password
     pw_box.send_keys(password)
     # click "Sign In"
-    browser.find_element(By.ID, "okta-signin-submit").click()
+    get_element(browser, "okta-signin-submit").click()
     
     # need to deal with email verification
 
@@ -43,22 +51,23 @@ def check_online_banking(user_ID, password):
     # go to GTKYCU's website
     browser.get('https://gtkycu.com/')
 
-    # don't do anything until the page has loaded properly
+    # get the user ID
+    user_ID = get_element(browser, NAME, "UserName")
+    # get the password
+    pw = get_element(browser, NAME, "Password")
+    
+# automatically dial main line
+def check_phone_status(username, password):
+    browser.get('https://login.windstream.com/idp/startSSO.ping?PartnerSpId=https%3A%2F%2Flogin.windstream.com&loginAdapterId=myBusinessWeb&TargetResource=https://mybusiness.gokinetic.com/account/pingauth/po3login')
+
+     # don't do anything until the page has loaded properly
     # we can tell it's loaded when the User ID is clickable
     # if it's not loaded after 4 seconds, throw TimeOutException
     wait = WebDriverWait(browser, 4)
-    element = wait.until(EC.element_to_be_clickable((By.NAME, "UserName")))
-
-
-
-
-
-
-
-
-
+    element = wait.until(EC.presence_of_element_located((By.NAME, "UserName")))
 
 def main():
-    participation_tracking("jbstuart", "Frosty-Shelving83")
-    
+    #participation_tracking("jbstuart", "Frosty-Shelving83")
+    check_online_banking("TestAccount2", "Twisty-Denial-Hymn80")
+
 main()
